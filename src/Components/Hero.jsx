6a0e6fb7f4878170972757a42dded947ed20f4b8
Pheletso8 <pheletso.marumoloe8@gmail.com>
2025-11-12 08:47:21 +0200

    fixed copilot mistakes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,44 +1,40 @@
 import idPhoto from '../assets/IMG_1362.jpeg'
-import { FaLinkedin } from "react-icons/fa";
-import { FaSquareGithub } from "react-icons/fa6";
-import NavBar from './NavBar';
+import { Linkedin } from 'lucide-react';
+import { Github } from 'lucide-react';
 
 export default function Hero() {
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-[#f9f9f9] to-[#ffffff] p-4 flex flex-col">
+    <div id='home' className="min-h-screen bg-gradient-to-r from-[#f9f9f9] to-[#ffffff] p-4 flex flex-col">
         <div className="flex flex-1 items-center justify-center">
-            <div className='flex flex-col items-center md:flex-row md:items-center md:justify-center'>
-                <img src={idPhoto} alt="ID like photo of me here" className='rounded-2xl object-contain mt-6 w-70 border-4 border-[#5b3e31] shadow-lg shadow-blue-600'/>
+            <div className='flex flex-col md:gap-8 lg:gap-20 items-center md:flex-row md:items-center md:justify-center'>
+                <img src={idPhoto} alt="ID like photo of me here" className='rounded-2xl object-contain mt-6 w-70 border-4 border-[#bc9b5e] shadow-xl shadow-blue-300'/>
                 <div className='flex flex-col items-center md:ml-8'>
                 <h3 className='text-lg text-[#5b3e31] font-semibold my-4 '>Hello, I'm</h3>
-                <h1 className='text-4xl font-bold text-[#5b3e31] mb-4'>Pheletso Marumoloe</h1>
-                <h2 className='text-blue-600 text-lg font-medium mb-8 mx-4'>
+                <h1 className='text-4xl font-bold text-[#5b3e31] mb-4 md:text-4xl lg:text-6xl'>Pheletso Marumoloe</h1>
+                <h2 className='text-blue-400 text-lg font-medium mb-8 mx-4'>
                     Data Scientist and Software Developer
                 </h2>
-                <a 
-                  href="https://www.linkedin.com/in/pheletso-marumoloe-422372281" 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <FaLinkedin 
-                    size={40} 
-                    color="#5b3e31" 
-                    className="transition hover:-translate-y-1 hover:scale-110 mb-2" 
-                  />
-                </a>
+                <p className='text-center mb-5 font-medium text-[#5b3e31] md:text-lg lg:text-xl'>Transforming complex data into actionable insights and<br /> building scalable software solutions.<br /> Specialized in machine learning, data pipelines, <br /> and full-stack development.</p>
+                <span className='flex gap-3 text-[#5b3e31]'>
+                  <a 
+                    href="https://www.linkedin.com/in/pheletso-marumoloe-422372281" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className='p-4 border-2 border-blue-600 rounded-full bg-blue-400 hover:bg-[#5b3e31] hover:border-blue-300 text-white transition-all duration-300 ease-in'
+                  >
+                    <Linkedin />
+                  </a>
 
-                <a 
-                  href="https://github.com/Pheletso8" 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  <FaSquareGithub 
-                    size={40} 
-                    color="#5b3e31" 
-                    className="transition hover:-translate-y-1 hover:scale-110" 
-                  />
-                </a>
+                  <a 
+                    href="https://github.com/Pheletso8" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className='p-4 border-2 border-blue-600 rounded-full bg-blue-400 hover:bg-[#5b3e31] hover:border-blue-300 text-white transition-all duration-300 ease-in'
+                  >
+                    <Github />
+                  </a>
+                </span>
                 </div>
             </div>
         </div>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,20 +3,22 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function NavBar() {
   return (
-    <div className=''>
+    <>
 
-        {/* Desktop Nav Bar */}
-        <div className='md:flex items-center md:px-[10rem] lg:px-[20rem] justify-between px-[3rem] py-[1.3rem] hidden bg-linear-to-r from-[#D2B48C] to-[#E5D3B3]'>
-            <a href=""><img src={Logo} alt="Logo" className='w-[4rem] h-[4rem] rounded-full'/></a>
-            <div className='flex gap-4 text-xl font-semibold'>
-                <a href="/" className='text-[#5b3e31] p-2 hover:bg-blue-400 rounded-full hover:text-white/80 transition-colors duration-300'>Home</a>
-                <a href="#projects" className='text-[#5b3e31] p-2 hover:bg-blue-400 rounded-full hover:text-white/80 transition-colors duration-300'>Projects</a>
-                <a href="#experiences" className='text-[#5b3e31] p-2 hover:bg-blue-400 rounded-full hover:text-white/80 transition-colors duration-300'>Experience</a>
-            </div>
+        {/* Mobile Nav Bar */}
+        <div className='flex sticky top-0 z-50 items-center justify-between px-[3rem] pt-[1rem] md:hidden bg-[#5b3e31] p-2'>
+            <img src={Logo} alt="Logo" className='w-[3rem] h-[3rem] rounded-full'/>
+            <details className="dropdown">
+                <summary className="btn m-1 ml-[40px]"><GiHamburgerMenu size={30}/></summary>
+                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li><a className='hover:bg-[#5b3e31] hover:text-white font-semibold text-blue-600 hover:text-lg delay-150 ease-in-out transition-all duration-300' href='#projects'>PROJECTS</a></li>
+                    <li><a className='hover:bg-[#5b3e31] hover:text-white font-semibold text-blue-600 hover:text-lg delay-150 ease-in-out transition-all duration-300' href='#experiences'>EXPERIENCES</a></li>
+                </ul>
+            </details>
         </div>
 
         {/* Desktop Nav Bar */}
-        <div className='md:flex items-center md:px-[10rem] lg:px-[20rem] justify-between px-[3rem] py-[1.3rem] hidden bg-linear-to-r from-[#D2B48C] to-[#E5D3B3]'>
+        <div className='md:flex sticky top-0 z-50 items-center md:px-[10rem] lg:px-[20rem] justify-between px-[3rem] py-[1.3rem] hidden bg-linear-to-r from-[#D2B48C] to-[#E5D3B3]'>
             <a href=""><img src={Logo} alt="Logo" className='w-[4rem] h-[4rem] rounded-full'/></a>
             <div className='flex gap-4 text-xl font-semibold'>
                 <a href="#home" className='text-[#5b3e31] p-2 hover:bg-blue-400 rounded-full hover:text-white/80 transition-colors duration-300'>Home</a>
@@ -24,7 +26,7 @@ export default function NavBar() {
                 <a href="#experiences" className='text-[#5b3e31] p-2 hover:bg-blue-400 rounded-full hover:text-white/80 transition-colors duration-300'>Experience</a>
             </div>
         </div>
-    </div>
+    </>
   );
 }
 
